806eed628c1c89971a1eb8c063f8a4d9e5f49cce
Matt Sievers <msievers2@my.gcu.edu>
2020-08-24 20:49:55 -0500

    Assignment 5 Wack a mole app.

---
+++ StopWatchApp/StopWatchApp/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace StopWatchApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.displayTimerLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.elapsedTimeLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.scoreValueLabel = new System.Windows.Forms.Label();
+            this.gameBoardBtn = new System.Windows.Forms.Button();
+            this.targetBtn = new System.Windows.Forms.Button();
+            this.playAgainBtn = new System.Windows.Forms.Button();
+            this.highScoreLabel = new System.Windows.Forms.Label();
+            this.highScoreValueLabel = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.levelLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(629, 261);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(133, 34);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(629, 301);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(133, 33);
+            this.stopBtn.TabIndex = 1;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // displayTimerLabel
+            // 
+            this.displayTimerLabel.AutoSize = true;
+            this.displayTimerLabel.Location = new System.Drawing.Point(634, 72);
+            this.displayTimerLabel.Name = "displayTimerLabel";
+            this.displayTimerLabel.Size = new System.Drawing.Size(0, 20);
+            this.displayTimerLabel.TabIndex = 3;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 1000;
+            this.gameTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // elapsedTimeLabel
+            // 
+            this.elapsedTimeLabel.AutoSize = true;
+            this.elapsedTimeLabel.Location = new System.Drawing.Point(625, 33);
+            this.elapsedTimeLabel.Name = "elapsedTimeLabel";
+            this.elapsedTimeLabel.Size = new System.Drawing.Size(105, 20);
+            this.elapsedTimeLabel.TabIndex = 5;
+            this.elapsedTimeLabel.Text = "Elapsed Time";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(625, 115);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(51, 20);
+            this.scoreLabel.TabIndex = 6;
+            this.scoreLabel.Text = "Score";
+            // 
+            // scoreValueLabel
+            // 
+            this.scoreValueLabel.AutoSize = true;
+            this.scoreValueLabel.Location = new System.Drawing.Point(634, 148);
+            this.scoreValueLabel.Name = "scoreValueLabel";
+            this.scoreValueLabel.Size = new System.Drawing.Size(0, 20);
+            this.scoreValueLabel.TabIndex = 7;
+            // 
+            // gameBoardBtn
+            // 
+            this.gameBoardBtn.BackColor = System.Drawing.Color.White;
+            this.gameBoardBtn.ImageIndex = 0;
+            this.gameBoardBtn.Location = new System.Drawing.Point(12, 12);
+            this.gameBoardBtn.Name = "gameBoardBtn";
+            this.gameBoardBtn.Size = new System.Drawing.Size(592, 514);
+            this.gameBoardBtn.TabIndex = 5;
+            this.gameBoardBtn.UseVisualStyleBackColor = false;
+            this.gameBoardBtn.Click += new System.EventHandler(this.gameBoardBtn_Click);
+            // 
+            // targetBtn
+            // 
+            this.targetBtn.Location = new System.Drawing.Point(236, 218);
+            this.targetBtn.Name = "targetBtn";
+            this.targetBtn.Size = new System.Drawing.Size(100, 100);
+            this.targetBtn.TabIndex = 4;
+            this.targetBtn.Text = "Target";
+            this.targetBtn.UseVisualStyleBackColor = true;
+            this.targetBtn.Click += new System.EventHandler(this.targetBtn_Click);
+            // 
+            // playAgainBtn
+            // 
+            this.playAgainBtn.BackColor = System.Drawing.Color.Green;
+            this.playAgainBtn.ForeColor = System.Drawing.Color.White;
+            this.playAgainBtn.Location = new System.Drawing.Point(629, 375);
+            this.playAgainBtn.Name = "playAgainBtn";
+            this.playAgainBtn.Size = new System.Drawing.Size(132, 92);
+            this.playAgainBtn.TabIndex = 8;
+            this.playAgainBtn.Text = "Play Again!";
+            this.playAgainBtn.UseVisualStyleBackColor = false;
+            this.playAgainBtn.Visible = false;
+            this.playAgainBtn.Click += new System.EventHandler(this.playAgainBtn_Click);
+            // 
+            // highScoreLabel
+            // 
+            this.highScoreLabel.AutoSize = true;
+            this.highScoreLabel.Location = new System.Drawing.Point(625, 177);
+            this.highScoreLabel.Name = "highScoreLabel";
+            this.highScoreLabel.Size = new System.Drawing.Size(88, 20);
+            this.highScoreLabel.TabIndex = 10;
+            this.highScoreLabel.Text = "High Score";
+            // 
+            // highScoreValueLabel
+            // 
+            this.highScoreValueLabel.AutoSize = true;
+            this.highScoreValueLabel.Location = new System.Drawing.Point(634, 206);
+            this.highScoreValueLabel.Name = "highScoreValueLabel";
+            this.highScoreValueLabel.Size = new System.Drawing.Size(0, 20);
+            this.highScoreValueLabel.TabIndex = 11;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Untitled.png");
+            this.imageList1.Images.SetKeyName(1, "");
+            this.imageList1.Images.SetKeyName(2, "");
+            this.imageList1.Images.SetKeyName(3, "");
+            this.imageList1.Images.SetKeyName(4, "");
+            // 
+            // levelLabel
+            // 
+            this.levelLabel.AutoSize = true;
+            this.levelLabel.Location = new System.Drawing.Point(629, 505);
+            this.levelLabel.Name = "levelLabel";
+            this.levelLabel.Size = new System.Drawing.Size(0, 20);
+            this.levelLabel.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Controls.Add(this.levelLabel);
+            this.Controls.Add(this.highScoreValueLabel);
+            this.Controls.Add(this.highScoreLabel);
+            this.Controls.Add(this.playAgainBtn);
+            this.Controls.Add(this.targetBtn);
+            this.Controls.Add(this.gameBoardBtn);
+            this.Controls.Add(this.scoreValueLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.elapsedTimeLabel);
+            this.Controls.Add(this.displayTimerLabel);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Label displayTimerLabel;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label elapsedTimeLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label scoreValueLabel;
+        private System.Windows.Forms.Button gameBoardBtn;
+        private System.Windows.Forms.Button targetBtn;
+        private System.Windows.Forms.Button playAgainBtn;
+        private System.Windows.Forms.Label highScoreLabel;
+        private System.Windows.Forms.Label highScoreValueLabel;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label levelLabel;
+    }
+}
+
---
+++ StopWatchApp/StopWatchApp/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace StopWatchApp
+{
+    public partial class Form1 : Form
+    {
+        public static Stopwatch watch = new Stopwatch();
+        private Random random = new Random();
+        static private int score = 0;
+        private int misses = 0;
+        private int highScore = 0;
+        private int difficulty = 0;
+        static private int tBtnHeight = 100;
+        static private int tBtnWidth = 100;
+        string[] imageArray = new string[5] { @"C:\Users\xxxfa\Dropbox\GCU Work\CST-227\CST-227\StopWatchApp\StopWatchApp\Images\mole.png", 
+            @"C:\Users\xxxfa\Dropbox\GCU Work\CST-227\CST-227\StopWatchApp\StopWatchApp\Images\cap.png",
+            @"C:\Users\xxxfa\Dropbox\GCU Work\CST-227\CST-227\StopWatchApp\StopWatchApp\Images\hulk.png",
+            @"C:\Users\xxxfa\Dropbox\GCU Work\CST-227\CST-227\StopWatchApp\StopWatchApp\Images\download.png",
+            @"C:\Users\xxxfa\Dropbox\GCU Work\CST-227\CST-227\StopWatchApp\StopWatchApp\Images\1up.jpg" };
+        static private int level = 0;
+        
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            gameTimer.Enabled = true;
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            watch.Stop();
+            gameTimer.Enabled = false;
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            levelLabel.Text = "Level: " + (level + 1).ToString();
+            //Microsoft Doccumentation (n.d.) Stopwatch.Elapsed Property.  Retrieved from: https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch.elapsed?view=netcore-3.1
+            //Dropping milliseconds.
+            TimeSpan ts = watch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+            displayTimerLabel.Text = elapsedTime;
+            gameBoardBtn.BackgroundImage = new Bitmap(imageArray[level]);
+            gameBoardBtn.BackgroundImageLayout = ImageLayout.Center;
+            
+
+            if (gameTimer.Enabled && random.Next(0, 10) < 5)
+            {
+               
+               
+                int randomColor = random.Next(0, 4);
+                
+
+                if (randomColor > 0)
+                {
+                    targetBtn.Height = tBtnHeight;
+                    targetBtn.Width = tBtnWidth;
+                    targetBtn.Left = random.Next(0, (gameBoardBtn.Width - targetBtn.Width));
+                    targetBtn.Top = random.Next(0, (gameBoardBtn.Height - targetBtn.Height));
+                    targetBtn.BackColor = Color.Green;
+                    targetBtn.Text = "Click Me!!";
+                    if (targetBtn.Left < 0)
+                    {
+                        targetBtn.Left = 0;
+                    }
+
+                    if (targetBtn.Top < 0)
+                    {
+                        targetBtn.Top = 0;
+                    }
+                    targetBtn.Visible = true;
+                }
+                else
+                {
+                    targetBtn.BackColor = Color.Red;
+                    targetBtn.Text = "It's a Bomb!!";
+                }
+                
+                checkConditions(ts);
+            }
+
+        }
+
+        private void targetBtn_Click(object sender, EventArgs e)
+        {
+            
+            if (targetBtn.BackColor.Equals(Color.Green) && (tBtnHeight > 20 && tBtnWidth > 20) )
+            {
+                targetBtn.Visible = false;
+                score += 1;
+                difficulty = score * 5;
+                scoreValueLabel.Text = score.ToString();
+                tBtnHeight = tBtnHeight - difficulty;
+                tBtnWidth = tBtnWidth - difficulty;
+            }
+            else if(targetBtn.BackColor.Equals(Color.Green) && (tBtnHeight < 20 && tBtnWidth < 20))
+            {
+                targetBtn.Visible = false;
+                score += 1;
+            }
+            else
+            {
+                targetBtn.Visible = false;
+                score -= 5;
+                difficulty = score * 5;
+                scoreValueLabel.Text = score.ToString();
+                tBtnHeight = tBtnHeight + (difficulty * -1);
+                tBtnWidth = tBtnWidth + (difficulty * -1);
+            }
+            
+        }
+
+        private void gameBoardBtn_Click(object sender, EventArgs e)
+        {
+            score -= 1;
+            misses += 1;
+            scoreValueLabel.Text = score.ToString();
+        }
+
+        private void checkConditions(TimeSpan ts)
+        {
+            if (ts.Minutes > 0)
+            {
+                gameTimer.Enabled = false;
+                targetBtn.Visible = false;
+                MessageBox.Show("Game Over" + "\n" + "Your high score was: " + score);
+                playAgainBtn.Visible = true;
+
+            }
+            else if (misses > 3)
+            {
+                gameTimer.Enabled = false;
+                targetBtn.Visible = false;
+                MessageBox.Show("Game Over" + "\n" + "Your high score was: " + score);
+                playAgainBtn.Visible = true;
+            }
+            else if (score == 50)
+            {
+                gameTimer.Enabled = false;
+                targetBtn.Visible = false;
+                level += 1;
+                MessageBox.Show("Congratulations, you've won!!" + "\n" + "Your high score was: " + score);
+                playAgainBtn.Visible = true;
+            }else if(tBtnHeight <= 20 && tBtnWidth <= 20)
+            {
+                gameTimer.Enabled = false;
+                targetBtn.Visible = false;
+                level += 1;
+                MessageBox.Show("Congratulations, you've won!!" + "\n" + "Your high score was: " + score);
+                playAgainBtn.Visible = true;
+            }
+
+
+
+        }
+
+        private void playAgainBtn_Click(object sender, EventArgs e)
+        {
+            playAgainBtn.Visible = false;
+            watch.Reset();
+            TimeSpan ts = watch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+            displayTimerLabel.Text = elapsedTime;
+            score = 0;
+            misses = 0;
+            tBtnHeight = 100;
+            tBtnWidth = 100;
+            if(score > highScore)
+            {
+                highScore = score;
+                highScoreValueLabel.Text = highScore.ToString();
+            }
+        }
+    }
+}
