09f6f6227995983db6d846b78ef1a52349937acc
Matt Sievers <msievers2@my.gcu.edu>
2020-08-31 16:06:09 -0500

    Initial Commit for week 6 assignment to setup URL.

---
+++ TextFileDataAccessDemo/TextFileDataAccessDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TextFileDataAccessDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\demos\test.txt";
+
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
