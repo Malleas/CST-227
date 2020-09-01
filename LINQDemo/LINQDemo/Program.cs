ce8ede269367e41b5719040ea00c92d0e86cee13
Matt Sievers <msievers2@my.gcu.edu>
2020-08-31 21:12:36 -0500

    LINQ Demo and challenge.  Week 6 assignment part 2

---
+++ LINQDemo/LINQDemo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[] { 50, 95, 82, 85, 77, 62, 100, 99, 83, 42, 10 };
+            
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { firstName = "Matt", lastName = "Sievers", age = 38, grade = 98 });
+            students.Add(new Student() { firstName = "Bob", lastName = "Smith", age = 52, grade = 74 });
+            students.Add(new Student() { firstName = "Lisa", lastName = "Jones", age = 22, grade = 85 });
+            students.Add(new Student() { firstName = "Carlos", lastName = "Gomez", age = 29, grade = 52 });
+
+
+
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine("{0} scores is {1} ", student.firstName, student.grade);
+            }
+
+            Console.ReadLine();
+
+            var theBestStudents =
+                from student in students
+                where student.grade > 90
+                select student;
+
+            foreach(Student student in theBestStudents)
+            {
+                Console.WriteLine("{0} has the best oone of the best grades of a {1}", student.firstName, student.grade);
+            }
+
+            Console.ReadLine();
+
+            var sortedScores =
+                from student in students
+                orderby student.grade
+                select student;
+
+            foreach (Student student in sortedScores)
+            {
+                Console.WriteLine("One of the scores was {0}", student.grade);
+            }
+            Console.ReadLine();
+
+            var bStudents =
+                from student in students
+                where student.grade < 90 && student.grade > 80
+                select student;
+
+            foreach (Student student in bStudents)
+            {
+                Console.WriteLine("One of our B students is {0} with a grade of {1}", student.firstName, student.grade);
+            }
+            Console.ReadLine();
+
+            students.Sort();
+            Console.WriteLine("Sorted student list using the compareTo() in student class and sort() on the list");
+            foreach (Student student in students)
+            {
+                
+                Console.WriteLine(student);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LINQDemo/LINQDemo/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    class Student : IComparable<Student>
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public int grade { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            if (this.age == other.age)
+            {
+                return this.firstName.CompareTo(other.firstName);
+            }
+            else if (this.grade == other.grade)
+            {
+                return this.firstName.CompareTo(other.firstName);
+            }
+            return other.age.CompareTo(this.age);
+        }
+
+        public override string ToString()
+        {
+            return this.firstName + ","
+                + this.lastName + ","
+                + this.age.ToString() + ","
+                + this.grade.ToString();
+        }
+    }
+}
